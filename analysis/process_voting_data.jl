e82dfdf79bd4bafbf2043da2204d0f2bb4a2b94e
Simon Poulding <simon.poulding@gmail.com>
2017-05-06 13:44:27 +0200

    added Julia scripts to process voting data

---
+++ analysis/process_voting_data.jl
@@ -0,0 +1,39 @@
+using DataFrames
+
+function load_voting_data(filename::AbstractString, source::Symbol=:eurovisiontv)
+	if source == :eurovisiontv
+		readtable(joinpath("..", "voting_data", "eurovision_tv", filename))
+	else
+		error("Unknown source: $source")
+	end
+end
+
+function load_voting_data(year::Int, source::Symbol=:eurovisiontv)
+	if source == :eurovisiontv
+		load_voting_data("grand_final_votes_" * string(year) * ".csv")
+	else
+		error("Unknown source: $source")
+	end
+end
+
+# returns aggregate vote with columns :entrant and :televote
+function aggregate_televote_by_entrant(votingdf::DataFrame, source::Symbol=:eurovisiontv)
+	if source == :eurovisiontv
+		adf = by(votingdf, :To_country, df -> sum(df[:Televote_Points]))
+		names!(adf, [:entrant, :televote])
+	else
+		error("Unknown source: $source")
+	end	
+end
+
+function aggregate_televote_by_entrant(year::Int, source::Symbol=:eurovisiontv)
+	votingdf = load_voting_data(year, source)
+	if source == :eurovisiontv
+		adf = by(votingdf, :To_country, df -> sum(df[:Televote_Points]))
+		names!(adf, [:entrant, :televote])
+	else
+		error("Unknown source: $source")
+	end	
+end
+
+
